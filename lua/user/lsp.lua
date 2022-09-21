0e80ddf5dfebf5c79bba3ca1a1c88b9a4e73c794
Alexandr Ragulin <klodiys62@gmail.com>
2022-09-21 20:43:53 +0300

    fix: remove unused prolog lsp

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,21 +1,20 @@
-
-local configs = require 'lspconfig.configs'
-local util = require 'lspconfig.util'
-if not configs.prolog_lsp then
-  configs.prolog_lsp = {
-    default_config = {
-      cmd = {"swipl",
-             "-g", "use_module(library(lsp_server)).",
-             "-g", "lsp_server:main",
-             "-t", "halt",
-             "--", "stdio"};
-      filetypes = {"prolog"};
-      root_dir = util.root_pattern("pack.pl");
-    };
-  }
-end
-require'lspconfig'.prolog_lsp.setup{on_attach = attach}
-
+-- local configs = require 'lspconfig.configs'
+-- local util = require 'lspconfig.util'
+-- if not configs.prolog_lsp then
+--   configs.prolog_lsp = {
+--     default_config = {
+--       cmd = {"swipl",
+--              "-g", "use_module(library(lsp_server)).",
+--              "-g", "lsp_server:main",
+--              "-t", "halt",
+--              "--", "stdio"};
+--       filetypes = {"prolog"};
+--       root_dir = util.root_pattern("pack.pl");
+--     };
+--   }
+-- end
+-- require'lspconfig'.prolog_lsp.setup{on_attach = attach}
+--
 local mason_status_ok, mason = pcall(require, 'mason')
 if not mason_status_ok then
     print("Mason failed to run")
