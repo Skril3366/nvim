3ae34f939f77a060e36703033f704dee23b419e6
Alexandr Ragulin <klodiys62@gmail.com>
2022-10-16 15:40:25 +0300

    change jdtls lsp settings

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -155,7 +155,7 @@ local config = {
                 -- }
             },
             progressReports = {
-                enabled = false,
+                enabled = true,
             },
         },
         signatureHelp = { enabled = true },
@@ -214,6 +214,6 @@ jdtls.start_or_attach(config)
 vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)"
 vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)"
 vim.cmd "command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()"
--- vim.cmd "command! -buffer JdtJol lua require('jdtls').jol()"
+vim.cmd "command! -buffer JdtJol lua require('jdtls').jol()"
 vim.cmd "command! -buffer JdtBytecode lua require('jdtls').javap()"
--- vim.cmd "command! -buffer JdtJshell lua require('jdtls').jshell()"
+vim.cmd "command! -buffer JdtJshell lua require('jdtls').jshell()"
