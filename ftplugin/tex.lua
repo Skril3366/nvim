6afd458e87f546e2b3b2852659a36407e665e15c
Alexandr Ragulin <klodiys62@gmail.com>
2022-10-18 17:54:33 +0300

    add compile on save file for LaTeX

---
+++ ftplugin/tex.lua
@@ -0,0 +1,9 @@
+-- Automatically compile latex file on save
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = { "*.tex" },
+    callback = function()
+        local file = vim.fn.glob("%:p")
+        vim.fn.jobstart("pdflatex '" .. file .. "'")
+    end,
+    group = vim.api.nvim_create_augroup("Latex Compile On Save", { clear = true }),
+})
