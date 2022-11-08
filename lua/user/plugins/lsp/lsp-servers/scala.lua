841cdc8e52fca661a40e43ef0fbb273f577623f1
a.ragulin <a.ragulin@macbook-a.ragulin>
2022-11-08 14:56:31 +0300

    enable more features of scala metals

--- lua/user/plugins/lsp/lsp-servers/scala.lua
+++ lua/user/plugins/lsp/lsp-servers/scala.lua
@@ -6,28 +6,32 @@
 local metals_config = require("metals").bare_config()
 
 metals_config.settings = {
-    showImplicitArguments = true,
-    superMethodLensesEnabled = true,
+  showImplicitArguments = true,
+  superMethodLensesEnabled = true,
+  showInferredType = false,
+  showImplicitConversionsAndClasses = false,
 }
 
 metals_config.on_attach = function(client, bufnr)
-    require("metals").setup_dap()
+  require("metals").setup_dap()
 end
 
-local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = {
-        "scala",
-        "sbt",
-        -- "java"
-    },
-    callback = function()
-        require("metals").initialize_or_attach(metals_config)
-    end,
-    group = nvim_metals_group,
+  pattern = {
+    "scala",
+    "sbt",
+    -- "java"
+  },
+  callback = function()
+    require("metals").initialize_or_attach(metals_config)
+  end,
+  group = vim.api.nvim_create_augroup("nvim-metals", { clear = true }),
 })
+
+metals_config.handlers["textDocument/publishDiagnostics"] =
+vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = { prefix = "" } })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
