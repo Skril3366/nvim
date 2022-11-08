4b214c81777bfabf5ccf88e112dc8ea2cfda2ae3
a.ragulin <a.ragulin@macbook-a.ragulin>
2022-11-08 14:56:40 +0300

    add todo-comments plugin

--- lua/user/core/keybindings.lua
+++ lua/user/core/keybindings.lua
@@ -6,14 +6,16 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap.set
 local opts = {
-    noremap = true,
-    silent = true
+  noremap = true,
+  silent = true,
 }
 
 ------------------------------- LSP -------------------------------------------
 local buf = vim.lsp.buf
 local diagnostic = vim.diagnostic
-keymap("n", "<leader>f", function() buf.format() end, opts)
+keymap("n", "<leader>f", function()
+  buf.format()
+end, opts)
 keymap("n", "<leader>a", buf.code_action, opts)
 keymap("n", "gd", buf.definition, opts)
 keymap("n", "gD", buf.declaration, opts)
@@ -25,160 +27,171 @@ keymap("n", "<leader>dp", diagnostic.goto_next, opts)
 keymap("n", "<leader>d", diagnostic.open_float, opts)
 
 ------------------------------- Telescope --------------------------------------
-local ok, telescope = pcall(require, 'telescope.builtin')
+local tb_ok, telescope_builtin = pcall(require, "telescope.builtin")
 
-if not ok then
-    print("Telescope is not installed")
+local t_ok, telescope = pcall(require, "telescope")
+
+if not tb_ok and not t_ok then
+  print("Telescope is not installed")
 else
-    local find_config_files = function()
-        telescope.find_files({ cwd = os.getenv("HOME") .. "/.config/nvim" })
-    end
-    -- Files
-    keymap("n", "<leader>m", find_config_files, opts)
-    keymap("n", "<leader>o", telescope.find_files, opts)
-    keymap("n", "<leader>gf", telescope.git_files, opts)
+  local find_config_files = function()
+    telescope_builtin.find_files({ cwd = os.getenv("HOME") .. "/.config/nvim" })
+  end
+  -- Files
+  keymap("n", "<leader>m", find_config_files, opts)
+  keymap("n", "<leader>o", telescope_builtin.find_files, opts)
+  keymap("n", "<leader>gf", telescope_builtin.git_files, opts)
 
-    -- Inside files
-    keymap("n", "<leader>s", telescope.current_buffer_fuzzy_find, opts)
-    keymap("n", "<leader>gg", telescope.live_grep, opts)
+  -- Inside files
+  keymap("n", "<leader>s", telescope_builtin.current_buffer_fuzzy_find, opts)
+  keymap("n", "<leader>gg", telescope_builtin.live_grep, opts)
 
-    -- Git
-    keymap("n", "<leader>gc", telescope.git_commits, opts)
-    keymap("n", "<leader>gb", telescope.git_branches, opts)
+  -- Git
+  keymap("n", "<leader>gc", telescope_builtin.git_commits, opts)
+  keymap("n", "<leader>gb", telescope_builtin.git_branches, opts)
 
-    -- LSP
-    keymap("n", "<leader>w", telescope.lsp_document_symbols, opts)
-    keymap("n", "gR", telescope.lsp_references, opts)
+  -- LSP
+  keymap("n", "<leader>w", telescope_builtin.lsp_document_symbols, opts)
+  keymap("n", "gR", telescope_builtin.lsp_references, opts)
 
-    -- Help
-    keymap("n", "<leader>H", telescope.help_tags, opts)
-    keymap("n", "<leader>K", telescope.keymaps, opts)
+  -- Help
+  keymap("n", "<leader>H", telescope_builtin.help_tags, opts)
+  keymap("n", "<leader>K", telescope_builtin.keymaps, opts)
 
-    -- Other
-    keymap("n", "<leader>D", telescope.diagnostics, opts)
-    keymap("n", "<leader>b", telescope.buffers, opts)
-    keymap("n", "<leader>M", "<cmd>Telescope harpoon marks<cr>", opts)
+  -- Other
+  keymap("n", "<leader>D", telescope_builtin.diagnostics, opts)
+  keymap("n", "<leader>b", telescope_builtin.buffers, opts)
+  keymap("n", "<leader>M", "<cmd>Telescope harpoon marks<cr>", opts)
 
-    -- Project
-    local ok, _ = pcall(telescope.load_extension, 'projects')
-    if ok then
-        print("Project is not installed")
-    else
-        keymap("n", "<leader>p", "<cmd>Telescope projects<cr>", opts)
-    end
+  -- Project
+  -- telescope.load_extension("projects")
+  local ok, _ = pcall(telescope.load_extension, "projects")
+  if not ok then
+    print("Project is not installed")
+  else
+    keymap("n", "<leader>p", "<cmd>Telescope projects<cr>", opts)
+  end
+
+  local ok, _ = pcall(require, "todo-comments")
+  if not ok then
+    print("Todo comments plugin is not installed")
+  else
+    keymap("n", "<leader>T", "<cmd>TodoTelescope<cr>", opts)
+  end
 end
 
 ------------------------------ Harpoon -----------------------------------------
-local hm_ok, harpoon_mark = pcall(require, 'harpoon.mark')
-local hu_ok, harpoon_ui = pcall(require, 'harpoon.ui')
+local hm_ok, harpoon_mark = pcall(require, "harpoon.mark")
+local hu_ok, harpoon_ui = pcall(require, "harpoon.ui")
 
 if not (hm_ok or hu_ok) then
-    print("Harpoon is not installed")
+  print("Harpoon is not installed")
 else
-    keymap("n", "<leader>h", harpoon_mark.add_file, opts)
-    keymap("n", "<leader>U", harpoon_ui.toggle_quick_menu, opts)
-    keymap("n", "<leader>n", function() harpoon_ui.nav_file(1) end, opts)
-    keymap("n", "<leader>e", function() harpoon_ui.nav_file(2) end, opts)
-    keymap("n", "<leader>t", function() harpoon_ui.nav_file(3) end, opts)
+  keymap("n", "<leader>h", harpoon_mark.add_file, opts)
+  keymap("n", "<leader>U", harpoon_ui.toggle_quick_menu, opts)
+  keymap("n", "<leader>n", function()
+    harpoon_ui.nav_file(1)
+  end, opts)
+  keymap("n", "<leader>e", function()
+    harpoon_ui.nav_file(2)
+  end, opts)
+  keymap("n", "<leader>t", function()
+    harpoon_ui.nav_file(3)
+  end, opts)
 end
 
-
 ------------------------------- DAP -------------------------------------------
-local ok, dap = pcall(require, 'dap')
+local ok, dap = pcall(require, "dap")
 
 if not ok then
-    print("Dap is not installed")
+  print("Dap is not installed")
 else
-    -- Steps
-    keymap("n", "<leader>dc", dap.continue, opts)
-    keymap("n", "<leader>di", dap.step_into, opts)
-    keymap("n", "<leader>dO", dap.step_out, opts)
-    keymap("n", "<leader>do", dap.step_over, opts)
+  -- Steps
+  keymap("n", "<leader>dc", dap.continue, opts)
+  keymap("n", "<leader>di", dap.step_into, opts)
+  keymap("n", "<leader>dO", dap.step_out, opts)
+  keymap("n", "<leader>do", dap.step_over, opts)
 
-    -- Breakpoints
-    local conditional_breakpoint = function()
-        dap.toggle_breakpoint(vim.fn.input('Condition: '))
-    end
-    keymap("n", "<leader>dB", conditional_breakpoint, opts)
-    keymap("n", "<leader>db", dap.toggle_breakpoint, opts)
+  -- Breakpoints
+  local conditional_breakpoint = function()
+    dap.toggle_breakpoint(vim.fn.input("Condition: "))
+  end
+  keymap("n", "<leader>dB", conditional_breakpoint, opts)
+  keymap("n", "<leader>db", dap.toggle_breakpoint, opts)
 
-    -- Other
-    keymap("n", "<leader>dt", dap.terminate, opts)
+  -- Other
+  keymap("n", "<leader>dt", dap.terminate, opts)
 
-    -- UI
-    local ok, dapui = pcall(require, 'dapui')
-    if not ok then
-        print("DapUI is not installed")
-    else
-        keymap("n", "<leader>du", dapui.toggle, opts)
-    end
+  -- UI
+  local ok, dapui = pcall(require, "dapui")
+  if not ok then
+    print("DapUI is not installed")
+  else
+    keymap("n", "<leader>du", dapui.toggle, opts)
+  end
 end
 
 ------------------------------- LuaSnip ---------------------------------------
-local ok, luasnip = pcall(require, 'luasnip')
+local ok, luasnip = pcall(require, "luasnip")
 
 if not ok then
-    print("LuaSnip is not installed")
+  print("LuaSnip is not installed")
 else
-    keymap({ "i", "s" }, "<C-k>", function()
-        if luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-        end
-    end,
-        opts)
-    keymap({ "i", "s" }, "<C-j>", function()
-        if luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-        end
-    end,
-        opts)
-    keymap({ "i", "s" }, "<C-l>", function()
-        if luasnip.choice_active() then
-            luasnip.change_choice(1)
-        end
-    end,
-        opts)
+  keymap({ "i", "s" }, "<C-k>", function()
+    if luasnip.expand_or_jumpable() then
+      luasnip.expand_or_jump()
+    end
+  end, opts)
+  keymap({ "i", "s" }, "<C-j>", function()
+    if luasnip.jumpable(-1) then
+      luasnip.jump(-1)
+    end
+  end, opts)
+  keymap({ "i", "s" }, "<C-l>", function()
+    if luasnip.choice_active() then
+      luasnip.change_choice(1)
+    end
+  end, opts)
 end
 
 ------------------------------- NvimTree ---------------------------------------
 
-local ok, nvim_tree = pcall(require, 'nvim-tree')
+local ok, nvim_tree = pcall(require, "nvim-tree")
 
 if not ok then
-    print("NvimTree is not installed")
+  print("NvimTree is not installed")
 else
-    keymap("n", "<leader>t", function()
-        nvim_tree.toggle()
-    end,
-        opts)
+  keymap("n", "<leader>t", function()
+    nvim_tree.toggle()
+  end, opts)
 end
 
 ------------------------------- Other -----------------------------------------
-keymap("n", "<leader>u", '<cmd>UndotreeToggle<cr><cmd>UndotreeFocus<cr>', opts)
-keymap("n", "<leader>i", '<cmd>cd %:p:h<cr>', opts)
+keymap("n", "<leader>u", "<cmd>UndotreeToggle<cr><cmd>UndotreeFocus<cr>", opts)
+keymap("n", "<leader>i", "<cmd>cd %:p:h<cr>", opts)
 -- keymap("n", "<leader>t", function ()
 --     vim.cmd("vimgrep /\<TODO\>/j **/*.*")
 --     vim.cmd("scope")
 -- end, opts)
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "latex", "tex" },
-    callback = function()
-        keymap("n", "<leader>c", '<cmd>!pdflatex %<cr>', opts)
-    end
+  pattern = { "latex", "tex" },
+  callback = function()
+    keymap("n", "<leader>c", "<cmd>!pdflatex %<cr>", opts)
+  end,
 })
 
 ---------------------------- Compile and run ----------------------------------
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "go" },
-    callback = function()
-        keymap("n", "<leader>c", '<cmd>!go run .<cr>', opts)
-    end
+  pattern = { "go" },
+  callback = function()
+    keymap("n", "<leader>c", "<cmd>!go run .<cr>", opts)
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "*.cpp" },
-    callback = function()
-        keymap("n", "<leader>c", '!g++ %; ./a.out', opts)
-    end
+  pattern = { "*.cpp" },
+  callback = function()
+    keymap("n", "<leader>c", "!g++ %; ./a.out", opts)
+  end,
 })
--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,219 +1,217 @@
-local status_ok, configs = pcall(require, 'nvim-treesitter.configs')
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-    print("Tree sitter failed to run")
-    return
+  print("Tree sitter failed to run")
+  return
 end
 
-configs.setup {
-    ensure_installed = {
-        "cpp",
-        "c",
-        "lua",
-        "go",
-        "scala",
-        -- Scripting languages
-        "bash",
-        -- Markup languages
-        "yaml",
-        "latex",
-        "json",
-        "markdown",
+configs.setup({
+  ensure_installed = {
+    "cpp",
+    "c",
+    "lua",
+    "go",
+    "scala",
+    -- Scripting languages
+    "bash",
+    -- Markup languages
+    "yaml",
+    "latex",
+    "json",
+    "markdown",
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
     },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-        },
-    },
-    indent = {
-        enable = true
-    },
-    highlight = {
-        enable = true,
-    },
-    textobjects = {
-        select = {
-            enable = true,
+  },
+  indent = {
+    enable = true,
+  },
+  highlight = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
 
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
 
-            keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ab"] = "@block.outer",
-                ["ib"] = "@block.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-            },
-        },
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
     },
-    refactor = {
-        highlight_definitions = {
-            enable = true,
-            -- Set to false if you have an `updatetime` of ~100.
-            clear_on_cursor_move = true,
-        },
-        highlight_current_scope = { enable = true },
-        smart_rename = {
-            enable = false,
-            -- keymaps = {
-            --     smart_rename = "gr",
-            -- },
-        },
-        navigation = {
-            enable = false,
-            -- keymaps = {
-            --     -- goto_definition = "gd",
-            --     list_definitions = "gnD",
-            --     list_definitions_toc = "gO",
-            -- },
-        },
+  },
+  refactor = {
+    highlight_definitions = {
+      enable = true,
+      -- Set to false if you have an `updatetime` of ~100.
+      clear_on_cursor_move = true,
     },
-    rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
-    }
-}
+    highlight_current_scope = { enable = true },
+    -- smart_rename = {
+    --   enable = true,
+    --   keymaps = { smart_rename = "gr" },
+    -- },
+    navigation = {
+      enable = false,
+      -- keymaps = {
+      --     -- goto_definition = "gd",
+      --     list_definitions = "gnD",
+      --     list_definitions_toc = "gO",
+      -- },
+    },
+  },
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  },
+})
 
-local status_ok, spellsitter = pcall(require, 'nvim-treesitter.configs')
+local status_ok, spellsitter = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-    print("Spell sitter is not installed")
+  print("Spell sitter is not installed")
 else
-    spellsitter.setup {
-        -- Whether enabled, can be a list of filetypes, e.g. {'python', 'lua'}
-        enable = true,
-        debug = false
-    }
+  spellsitter.setup({
+    -- Whether enabled, can be a list of filetypes, e.g. {'python', 'lua'}
+    enable = true,
+    debug = false,
+  })
 end
 
 local spellsitter_group = vim.api.nvim_create_augroup("spellsitter", { clear = true })
 
 vim.api.nvim_create_autocmd("TermOpen", {
-    pattern = "*", -- disable spellchecking in the embeded terminal
-    command = "setlocal nospell",
-    group = spellsitter_group,
+  pattern = "*", -- disable spellchecking in the embeded terminal
+  command = "setlocal nospell",
+  group = spellsitter_group,
 })
 
-require 'treesitter-context'.setup {
-    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-    trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-    min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-        -- For all filetypes
-        -- Note that setting an entry here replaces all other patterns for this entry.
-        -- By setting the 'default' entry below, you can control which nodes you want to
-        -- appear in the context window.
-        default = {
-            'class',
-            'function',
-            'method',
-            'for',
-            'while',
-            'if',
-            'switch',
-            'case',
-        },
-        -- Patterns for specific filetypes
-        -- If a pattern is missing, *open a PR* so everyone can benefit.
-        tex = {
-            'chapter',
-            'section',
-            'subsection',
-            'subsubsection',
-        },
-        rust = {
-            'impl_item',
-            'struct',
-            'enum',
-        },
-        scala = {
-            'object_definition',
-        },
-        vhdl = {
-            'process_statement',
-            'architecture_body',
-            'entity_declaration',
-        },
-        markdown = {
-            'section',
-        },
-        elixir = {
-            'anonymous_function',
-            'arguments',
-            'block',
-            'do_block',
-            'list',
-            'map',
-            'tuple',
-            'quoted_content',
-        },
-        json = {
-            'pair',
-        },
-        yaml = {
-            'block_mapping_pair',
-        },
+require("treesitter-context").setup({
+  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+    -- For all filetypes
+    -- Note that setting an entry here replaces all other patterns for this entry.
+    -- By setting the 'default' entry below, you can control which nodes you want to
+    -- appear in the context window.
+    default = {
+      "class",
+      "function",
+      "method",
+      "for",
+      "while",
+      "if",
+      "switch",
+      "case",
     },
-    exact_patterns = {
-        -- Example for a specific filetype with Lua patterns
-        -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-        -- exactly match "impl_item" only)
-        -- rust = true,
+    -- Patterns for specific filetypes
+    -- If a pattern is missing, *open a PR* so everyone can benefit.
+    tex = {
+      "chapter",
+      "section",
+      "subsection",
+      "subsubsection",
     },
+    rust = {
+      "impl_item",
+      "struct",
+      "enum",
+    },
+    scala = {
+      "object_definition",
+    },
+    vhdl = {
+      "process_statement",
+      "architecture_body",
+      "entity_declaration",
+    },
+    markdown = {
+      "section",
+    },
+    elixir = {
+      "anonymous_function",
+      "arguments",
+      "block",
+      "do_block",
+      "list",
+      "map",
+      "tuple",
+      "quoted_content",
+    },
+    json = {
+      "pair",
+    },
+    yaml = {
+      "block_mapping_pair",
+    },
+  },
+  exact_patterns = {
+    -- Example for a specific filetype with Lua patterns
+    -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
+    -- exactly match "impl_item" only)
+    -- rust = true,
+  },
 
-    -- [!] The options below are exposed but shouldn't require your attention,
-    --     you can safely ignore them.
+  -- [!] The options below are exposed but shouldn't require your attention,
+  --     you can safely ignore them.
 
-    zindex = 20, -- The Z-index of the context window
-    mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
-    -- Separator between context and content. Should be a single character string, like '-'.
-    -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-    separator = nil,
-    markid = { enable = true },
-}
+  zindex = 20, -- The Z-index of the context window
+  mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+  -- Separator between context and content. Should be a single character string, like '-'.
+  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  separator = nil,
+  markid = { enable = true },
+})
 
-local status_ok, markid = pcall(require, 'markid')
+local status_ok, markid = pcall(require, "markid")
 if not status_ok then
-    print("Markid is not installed")
-    return
+  print("Markid is not installed")
+  return
 end
 
 local markid_colors = {
-    medium = {
-        '#2ac3de',
-        '#73daca',
-        '#7aa2f7',
-        '#7dcfff',
-        -- '#9ece6a',
-        '#b4f9f8',
-        '#bb9af7',
-        '#e0af68',
-        '#f7768e',
-        '#ff9e64',
-    },
-    -- bright = { "#f5c0c0", "#f5d3c0", "#f5eac0", "#dff5c0", "#c0f5c8", "#c0f5f1", "#c0dbf5", "#ccc0f5", "#f2c0f5",
-    --     "#98fc03" },
-    -- dark = { "#c99d9d", "#c9a99d", "#c9b79d", "#c9c39d", "#bdc99d", "#a9c99d", "#9dc9b6", "#9dc2c9", "#9da9c9",
-    --     "#b29dc9" }
+  medium = {
+    "#2ac3de",
+    "#73daca",
+    "#7aa2f7",
+    "#7dcfff",
+    -- '#9ece6a',
+    "#b4f9f8",
+    "#bb9af7",
+    "#e0af68",
+    "#f7768e",
+    "#ff9e64",
+  },
+  -- bright = { "#f5c0c0", "#f5d3c0", "#f5eac0", "#dff5c0", "#c0f5c8", "#c0f5f1", "#c0dbf5", "#ccc0f5", "#f2c0f5",
+  --     "#98fc03" },
+  -- dark = { "#c99d9d", "#c9a99d", "#c9b79d", "#c9c39d", "#bdc99d", "#a9c99d", "#9dc9b6", "#9dc2c9", "#9da9c9",
+  --     "#b29dc9" }
 }
 
-require 'nvim-treesitter.configs'.setup {
-    markid = {
-        enable = true,
-        colors = markid_colors.medium,
-        queries = markid.queries,
-        is_supported = function(lang)
-            local queries = configs.get_module("markid").queries
-            return pcall(vim.treesitter.parse_query, lang, queries[lang] or queries['default'])
-        end
-    }
-}
+require("nvim-treesitter.configs").setup({
+  markid = {
+    enable = true,
+    colors = markid_colors.medium,
+    queries = markid.queries,
+    is_supported = function(lang)
+      local queries = configs.get_module("markid").queries
+      return pcall(vim.treesitter.parse_query, lang, queries[lang] or queries["default"])
+    end,
+  },
+})
