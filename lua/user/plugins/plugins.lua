188c61bfc4b49f52c8a2890d70fb2a5705d4da78
a.ragulin <a.ragulin@macbook-a.ragulin>
2022-11-08 10:31:51 +0300

    fix project_nvim root directory

--- lua/user/plugins/plugins.lua
+++ lua/user/plugins/plugins.lua
@@ -276,9 +276,12 @@ return packer.startup(function(use)
 		"ahmedkhalf/project.nvim",
 		config = function()
 			require("project_nvim").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
+				sync_root_with_cwd = true,
+				respect_buf_cwd = true,
+				update_focused_file = {
+					enable = true,
+					update_root = true,
+				},
 			})
 		end,
 	})
