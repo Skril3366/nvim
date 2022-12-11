424b6383fa91c525011ab16ac5bdd9d8860d508b
Alexandr Ragulin <klodiys62@gmail.com>
2022-12-11 17:08:50 +0300

    configure tex file behaviour

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -1,11 +1,16 @@
 -- Automatically compile latex file on save
 vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = { "*.tex" },
-    callback = function()
-        local file = vim.fn.glob("%:p")
-        vim.fn.jobstart("pdflatex '" .. file .. "'")
-    end,
-    group = vim.api.nvim_create_augroup("Latex Compile On Save", { clear = true }),
+  pattern = { "*.tex" },
+  callback = function()
+    local file = vim.fn.glob("%:p")
+    vim.fn.jobstart("pdflatex '" .. file .. "'")
+  end,
+  group = vim.api.nvim_create_augroup("Latex Compile On Save", { clear = true }),
 })
 
-vim.cmd("let g:vimtex_view_method = 'zathura'")
+print("Loaded tex.lua")
+
+vim.cmd([[
+let g:vimtex_view_method = 'zathura'
+let g:vimtex_format_enabled = 1
+]])
