91fec5f49e311c35a49f73001474b443686aee45
Alexandr Ragulin <a.ragulin@tinkoff.ru>
2023-12-02 18:09:45 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -14,20 +14,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 			brew install "$DEP"
 		done
 	else
-		echo "Brew could not be found"
-		echo "None of the dependencies are installed"
+		echo "Brew not found, you can install it from https://brew.sh/"
 		exit
 	fi
-	# elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	#         # ...
-	# elif [[ "$OSTYPE" == "cygwin" ]]; then
-	#         # POSIX compatibility layer and Linux environment emulation for Windows
-	# elif [[ "$OSTYPE" == "msys" ]]; then
-	#         # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-	# elif [[ "$OSTYPE" == "win32" ]]; then
-	#         # I'm not sure this can happen.
-	# elif [[ "$OSTYPE" == "freebsd"* ]]; then
-	#         # ...
-	# else
-	#         # Unknown.
 fi
