6d3de4c09b50703869acde1bc9d642ef72ea02ed
Alexandr Ragulin <a.ragulin@tinkoff.ru>
2026-03-02 10:31:38 +0300

    turn off render-markdown

--- lua/user/plugins/avante.lua
+++ lua/user/plugins/avante.lua
@@ -55,12 +55,12 @@ return {
         },
       },
     },
-    {
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
+    -- {
+    --   "MeanderingProgrammer/render-markdown.nvim",
+    --   opts = {
+    --     file_types = { "markdown", "Avante" },
+    --   },
+    --   ft = { "markdown", "Avante" },
+    -- },
   },
 }
